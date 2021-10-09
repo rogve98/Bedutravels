f4141f0c6581a1919ff8d0b1e48ac861b89c61f7
Rodrigo Vega <rockdrigo6@ciencias.unam.mx>
2021-10-09 16:15:52 -0500

    Agregando el template de login y sus estilos

--- tours/urls.py
+++ tours/urls.py
@@ -4,5 +4,7 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"),
+        name="login"), 
     path('api/graphql/', GraphQLView.as_view(graphiql=True)),
 ]
